c47a9a94562286a87175c8f5d4ee183825f11f8e
EldeenJohnson <77896084+EldeenJohnson@users.noreply.github.com>
2022-09-25 20:28:49 -0500

    if else statement

---
+++ ifstatement.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer - If Statement</title>
+</head>
+<body>   
+    <a href="index.php">Home</a>
+
+    <h3 style="color: teal">Exam Results</h3>
+    
+    <?php 
+    
+        //An if statement that will carry out an action based on the value of the variable
+
+        echo '<h2>IF Statements</h2>';
+
+          $grade =80;
+
+        if ($grade >= 50){
+            echo '<h3 style="color: green"> YOU HAVE PASSED</h3>';
+        }
+        else{
+            echo '<h3 style="color: red">YOU HAVE FAILED<h3>';
+        }
+        $grade ='B';
+        //If-Else IF-Else
+        if($grade == 'A'){
+            echo '<h2 style="color: green>YOU ARE A SUPERSTAR!</h2>';
+        }
+        elseif ($grade == 'B'){
+            echo '<h2 style="color: cyan>YOU DID WELL!</h2>';
+        }
+        else{
+            echo '<h2 style="color: red>YOU HAVE FAILED...</h2>';
+        }
+    ?>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,48 @@
-<?php
-echo 'Hello World!'
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer - Index</title>
+</head>
+<body>
+    <h2> Follow each link to the page example</h2>
+    <ul>
+        <li><a href="array.php">Simple Array and Printouts</a></li>
+        <li><a href="forloop.php">Simple For Loop</a></li>
+        <li><a href="dowhileloop.php">Simple Do While</a></li>
+        <li><a href="whileloop.php">Simple While Loop</a></li>
+        <li><a href="ifstatement.php">Simple If Statement</a></li>
+        <li><a href="switchstatement.php">Simple Switch Statement</a></li>
+    </ul>
+
+    <!-- Basic HTML -->
+    <h1>Hello HTML - PHP Primer</h1>
+
+    <?php 
+        //Printing to HTML using echo
+        echo 'Hello PHP!' ;
+        echo '<br/>';
+        echo 'Second Line';
+        echo '<br/>';
+        
+    ?>
+
+    <?php
+        // declare variable
+        $name = 'Eldeen Johnson';
+        $age = 36;
+
+    
+        // echo variable
+        echo $name;
+        echo '<h1>My Name is : '.$name.'</h1>';
+        echo '<h1>My age is : '.$age.'</h1>';
+        //Echo using double quotes and interpolation
+        echo "<h1>My Name is : $name </h1>";
+
+    ?>
+
+</body>
+</html>
