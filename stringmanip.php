a9a7411e50c94c0357517a9c41ec84fd336611a5
EldeenJohnson <77896084+EldeenJohnson@users.noreply.github.com>
2022-09-26 20:13:12 -0500

    string

--- stringmanip.php
+++ stringmanip.php
@@ -25,6 +25,33 @@
             echo 'Uppercase: ' .strtolower("THIS WAS ALL UPPER CASE").'<br/> ';
             echo '<hr/>';
             echo 'Repeat String: '.str_repeat('a',10).'<br/>';
+            echo 'Repeat String - Nested Function: '.strtoupper(str_repeat('a',10)).'<br/>';
+            echo 'get a Substring: ' .substr($name, 5, 10).'<br/>';
+
+            echo 'Get position of string: ' .strpos($name, 'j').'<br/>';
+            //retuns Null
+            //echo 'Get position of string: ' . ($combine, 'z').'<br/>';
+            echo 'Find Character "h": ' .strchr($name, 'h').'<br/>';
+            echo 'Find Character "l": ' .strchr($name, 'l').'<br/>';
+            echo 'Find Character "n": ' .strchr($name, 'n').'<br/>';
+            echo 'Find Character "j": ' .strchr($name, 'j').'<br/>';
+
+            echo 'Find lenght of a string: ' .strlen($name).'<br/>';
+
+            //triming spaces
+            echo 'Without Trim: '. "A" . " B C D " . "E" . '<br/>';
+            echo 'Trim spaces on both sides: '. "A" . trim(" B C D ") . "E" . '<br/>';
+            echo 'Trim spaces to the left: '. "A" . ltrim(" B C D ") . "E" . '<br/>';
+            echo 'Trim spaces to the right: '. "A" . rtrim(" B C D ") . "E" . '<br/>';
+
+            echo 'Replace string with another: '.str_replace("Stand", "Sit", $phrase2). '<br/>';
+
+            
+
+
+             
+
+
     ?>
     
 </body>
