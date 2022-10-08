4f3008f38355daf6ff019da8430411859cc99290
EldeenJohnson <77896084+EldeenJohnson@users.noreply.github.com>
2022-10-08 01:27:35 -0500

    update

--- index.php
+++ index.php
@@ -28,5 +28,8 @@
         echo "<h1>My Name is : $name </h1>";
 
     ?>
-    <button type="button" class="btn btn-success">Click Here</button>
+    <button type="button" class="btn btn-dark">Click Here</button>
+    <a href="https://imaginicreations.com" target="_blank" class="btn btn-primary">Imaginicreations.com</a>
+    <a href="https://www.instagram.com/imagini_creations/?igshid=YmMyMTA2M2Y%3D" target="_blank" class="btn btn-success">Photography</a>
+    <a href="https://www.heroku.com" target="_blank" class="btn btn-danger">Heroku.com</a>
 <?php require 'includes/footer.php' ?>
